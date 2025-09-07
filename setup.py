6b6b9562bf1bd102edc326ca42f94eab8dc9742d
idzingirai <ishe.dzingirai@gmail.com>
2025-09-07 17:24:02 +0000

    enhancement: Add versions to packages in setup file

--- setup.py
+++ setup.py
@@ -31,25 +31,30 @@ def read(fname):
 
 
 setuptools.setup(
-      name='textaugment',
-      version=__version__,
-      packages=setuptools.find_packages(exclude=('test*', )),
-      author='Joseph Sefara',
-      author_email='sefaratj@gmail.com',
-      license='MIT',
-      keywords=['text augmentation', 'python', 'natural language processing', 'nlp'],
-      url='https://github.com/dsfsi/textaugment',
-      description='A library for augmenting text for natural language processing applications.',
-      long_description=read("README.md"),
-      long_description_content_type="text/markdown",
-      install_requires=['nltk', 'gensim>=4.0', 'textblob', 'numpy', 'googletrans>=2'],
-      classifiers=[
-          "Intended Audience :: Developers",
-          "Natural Language :: English",
-          "License :: OSI Approved :: MIT License",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: Implementation :: PyPy",
-          "Topic :: Text Processing :: Linguistic",
-        ]
+    name='textaugment',
+    version=__version__,
+    packages=setuptools.find_packages(exclude=('test*', )),
+    author='Joseph Sefara',
+    author_email='sefaratj@gmail.com',
+    license='MIT',
+    keywords=['text augmentation', 'python', 'natural language processing', 'nlp'],
+    url='https://github.com/dsfsi/textaugment',
+    description='A library for augmenting text for natural language processing applications.',
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'gensim==4.3.3',
+        'googletrans==4.0.2',
+        'nltk==3.9.1',
+        'numpy==1.26.4'
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Text Processing :: Linguistic",
+    ]
 )
